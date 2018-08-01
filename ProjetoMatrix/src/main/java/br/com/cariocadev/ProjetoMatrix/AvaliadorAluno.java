546160c25a267767a3391c54e25c7c4301493817
Victor Chengue <victor_scb@hotmail.com>
2018-08-01 17:57:39 -0300

    correções e ajustes

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -6,59 +6,51 @@ public class AvaliadorAluno {
 
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
 		// Implementação o código aqui
-	
-		BigDecimal maiornota = new BigDecimal("0");
-		if(notas ==null) 
-	        throw new IllegalArgumentException();
-		for(int i=0;i<=3;i++)  {
-			if (notas[i].compareTo(maiornota) ==1)
-				maiornota = notas[i];		
-		} 		return maiornota.setScale(2);
 
+		BigDecimal maiornota = new BigDecimal("0");
+		if (notas == null)
+			throw new IllegalArgumentException();
+		for (int i = 0; i <= 3; i++) {
+			if (notas[i].compareTo(maiornota) == 1)
+				maiornota = notas[i];
 		}
-	
-	
+		return maiornota.setScale(2);
+
+	}
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
 		BigDecimal media = new BigDecimal("0");
 		BigDecimal divisor = new BigDecimal("3");
-		if((nota1 !=null && nota2 !=null && nota3 !=null) &&
-		(nota1.compareTo(new BigDecimal("10"))<=0 && 
-		nota2.compareTo(new BigDecimal("10"))<=0 && 
-		nota3.compareTo(new BigDecimal("10"))<=0) &&
-		(nota1.compareTo(new BigDecimal("0"))>0 && 
-		nota2.compareTo(new BigDecimal("0"))>0 && 
-		nota3.compareTo(new BigDecimal("0"))>0))
-				{
+		if ((nota1 != null && nota2 != null && nota3 != null)
+				&& (nota1.compareTo(new BigDecimal("10")) <= 0 && nota2.compareTo(new BigDecimal("10")) <= 0
+						&& nota3.compareTo(new BigDecimal("10")) <= 0)
+				&& (nota1.compareTo(new BigDecimal("0")) > 0 && nota2.compareTo(new BigDecimal("0")) > 0
+						&& nota3.compareTo(new BigDecimal("0")) > 0)) {
 
-			media=nota1.add(nota2).add(nota3).divide(divisor, BigDecimal.ROUND_HALF_UP);
-	
-	    	return media.setScale(2); 
-				}
-		else
-		{
+			media = nota1.add(nota2).add(nota3).divide(divisor, BigDecimal.ROUND_HALF_UP);
 
-        throw new IllegalArgumentException();
+			return media.setScale(2);
+		} else {
+
+			throw new IllegalArgumentException();
 		}
 
-		
 	}
 
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		 //Implementação o código aqui
-	   String resultado;
-	   BigDecimal pegarMedia = new BigDecimal("0");  
-       pegarMedia=getMedia(nota1,nota2,nota3);
-      
-      
-       if(pegarMedia.compareTo(new BigDecimal("6"))>=1) {
-		resultado="APROVADO";       }
-       else
-    	   if(pegarMedia.compareTo(new BigDecimal("4"))>=0) {
-   		resultado="PROVA_FINAL";       }
-       
-       else 
-    	   resultado="REPROVADO";       
-       return resultado;
+		// Implementação o código aqui
+		String resultado;
+		BigDecimal valorMedia = new BigDecimal("0");
+		valorMedia = getMedia(nota1, nota2, nota3);
+
+		if (valorMedia.compareTo(new BigDecimal("6")) >= 1) {
+			resultado = "APROVADO";
+		} else if (valorMedia.compareTo(new BigDecimal("4")) >= 0) {
+			resultado = "PROVA_FINAL";
+		}
+
+		else
+			resultado = "REPROVADO";
+		return resultado;
 	}
-} 
+}
